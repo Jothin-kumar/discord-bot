d8638ae8ad0ffc7624781ccf23b703fbca3e713d
Jothin Kumar <contact@jothin.tech>
2024-11-04 14:37:56 +0530

    add week

--- moderation_functions.py
+++ moderation_functions.py
@@ -19,6 +19,7 @@ async def ban(msg):
             await msg.reply(f"**I can't ban {user.mention}**\n\nKindly make sure that I have the necessary permissions and that the user isn't superior to me")
 
 async def mute(msg):
+    weeks = 0
     days = 0
     hrs = 0
     mins = 0
@@ -26,6 +27,8 @@ async def mute(msg):
     for word in "".join([l for l in msg.content if l.isalnum() or l == " "]).split()[::-1]:
         try:
             match word[-1]:
+                case "w":
+                    weeks = int(word[:-1])
                 case "d":
                     days = int(word[:-1])
                 case "h":
@@ -36,16 +39,16 @@ async def mute(msg):
                     secs = int(word[:-1])
         except ValueError:
             pass
-    if not any([days, hrs, mins, secs]):
+    if not any([weeks, days, hrs, mins, secs]):
         mins = 1
-    td = timedelta(days=days, hours=hrs, minutes=mins, seconds=secs)
+    td = timedelta(weeks=weeks, days=days, hours=hrs, minutes=mins, seconds=secs)
     if td.total_seconds() > 60*60*24*7*4:
         await msg.reply("I can't mute an user for more than 4 weeks")
         return
     for user in msg.mentions:
         try:
             await user.timeout(td, reason=msg.content)
-            await msg.reply(f"Muted {user.mention} for {f'{days} days ' if days else ''}{f'{hrs} hours ' if hrs else ''}{f'{mins} minutes ' if mins else ''}{f'{secs} seconds' if secs else ''}")
+            await msg.reply(f"Muted {user.mention} for {f'{weeks} weeks ' if weeks else ''}{f'{days} days ' if days else ''}{f'{hrs} hours ' if hrs else ''}{f'{mins} minutes ' if mins else ''}{f'{secs} seconds' if secs else ''}")
         except errors.Forbidden:
             await msg.reply(f"**I can't mute {user.mention}**\n\nKindly make sure that I have the necessary permissions and that the user isn't superior to me")
 
